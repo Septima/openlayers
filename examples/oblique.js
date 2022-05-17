98e0a46e12a5a850aec6ebb1d7784948574c7ce8
Nicolai Mogensen <nicolaimogensen@outlook.dk>
2022-05-17 12:59:24 +0200

    Opdater eksempel

--- examples/oblique.js
+++ examples/oblique.js
@@ -7,38 +7,23 @@ import proj4 from "proj4";
 import { getCenter } from "../src/ol/extent.js";
 import { register } from "../src/ol/proj/proj4.js";
 
-
-// In reality this does not matter as GeoTIFF.js nulls the projection...
-// It would however be nice if you could send a flag to use the supplied projection, with extent
-// Instead of assuming that the geoKeys attribute is present, when for oblique imagery it might not. 
-const projection = new Projection({
-  code: "oblique-image",
-  extent: [0,0,13470,8670],
-});
-
-const cogExtent = [0,0,13470,8670]; // Correspond to the width and height of the image [xmin, ymin, xmax, ymax] 
-const cogUrl =
-  "https://tmp-asger-public.s3.eu-central-1.amazonaws.com/2017_84_40_1_0015_00008062.cog.tif";
-const cog_layer =  new TileLayer({
-    source: new GeoTIFF({
-      sources: [
-        {
-          test: true, // To test stuff that wont break existing functionality
-          projection: projection,
-          url: cogUrl,
-        },
-      ],
-    }),
-    extent: cogExtent,
-  });
+const source = new GeoTIFF({
+  convertToRGB: true,
+  sources: [
+    {
+      url:
+        "https://api.dataforsyningen.dk/skraafoto_server_test/COG_oblique_2021/10km_613_58/1km_6132_583/2021_83_37_2_0025_00001961.tif?token=4adf32524ae6d6998565f638a1090ba1",
+      nodata: 0
+    }
+  ]
+})
 const map = new Map({
   target: "map",
   layers: [
-    cog_layer,
+    new TileLayer({
+      source: source})
   ],
-  view: new View({
-    center: getCenter(cogExtent),
-    extent: cogExtent,
-    zoom: 12, //Not sure how this value correspond to relative zoom
-  }),
+  view: source.getView(),
 });
+
+console.log(source);
